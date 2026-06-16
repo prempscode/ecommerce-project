4bb40d8cf1dc61c90aa304994ce6e90456c9e6dd
premdev <prempravash65@gmail.com>
2026-06-16 04:38:37 -0700

    Moved Orders and Tracking Page to React

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Routes } from "react-router";
 import { Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import CheckoutPage from "./pages/CheckoutPage";
+import OrdersPage from "./pages/OrdersPage";
+import TrackingPage from "./pages/TrackingPage";
 import "./App.css";
 
 function App() {
@@ -9,7 +11,9 @@ function App() {
     <>
       <Routes>
         <Route index element={<HomePage />}></Route>
-        <Route path="checkout" element={<CheckoutPage/>}></Route>
+        <Route path="checkout" element={<CheckoutPage />}></Route>
+        <Route path="tracking" element={<TrackingPage />}></Route>
+        <Route path="orders" element={<OrdersPage />}></Route>
       </Routes>
     </>
   );
