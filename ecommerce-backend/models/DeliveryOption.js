ebb9683f2cffbf1b113162b7feaebcafe6c3ef87
premdev <prempravash65@gmail.com>
2026-09-10 11:42:00 +0530

    Update DeliveryOptions.js

--- ecommerce-backend/models/DeliveryOption.js
+++ ecommerce-backend/models/DeliveryOption.js
@@ -1,27 +1,31 @@
-import { DataTypes } from 'sequelize';
-import { sequelize } from './index.js';
+import { DataTypes } from 'sequelize'
+import { sequelize } from './index.js'
 
-export const DeliveryOption = sequelize.define('DeliveryOption', {
-  id: {
-    type: DataTypes.STRING,
-    primaryKey: true
+export const DeliveryOption = sequelize.define(
+  'DeliveryOption',
+  {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true
+    },
+    deliveryDays: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    priceCents: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE(3)
+    },
+    updatedAt: {
+      type: DataTypes.DATE(3)
+    }
   },
-  deliveryDays: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  priceCents: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  createdAt: {
-    type: DataTypes.DATE(3)
-  },
-  updatedAt: {
-    type: DataTypes.DATE(3)
-  },
-}, {
-  defaultScope: {
-    order: [['createdAt', 'ASC']]
+  {
+    defaultScope: {
+      order: [['createdAt', 'ASC']]
+    }
   }
-});
+)
