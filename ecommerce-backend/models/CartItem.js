67a9432f4932685cf1be2d02220f5ad4039b2bdc
premdev <prempravash65@gmail.com>
2026-09-10 11:42:00 +0530

    Update cart iterm.js

--- ecommerce-backend/models/CartItem.js
+++ ecommerce-backend/models/CartItem.js
@@ -1,35 +1,39 @@
-import { DataTypes } from 'sequelize';
-import { sequelize } from './index.js';
+import { DataTypes } from 'sequelize'
+import { sequelize } from './index.js'
 
-export const CartItem = sequelize.define('CartItem', {
-  productId: {
-    type: DataTypes.UUID,
-    allowNull: false,
-    references: {
-      model: 'Products',
-      key: 'id'
+export const CartItem = sequelize.define(
+  'CartItem',
+  {
+    productId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: 'Products',
+        key: 'id'
+      }
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    deliveryOptionId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'DeliveryOptions',
+        key: 'id'
+      }
+    },
+    createdAt: {
+      type: DataTypes.DATE(3)
+    },
+    updatedAt: {
+      type: DataTypes.DATE(3)
     }
   },
-  quantity: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  deliveryOptionId: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    references: {
-      model: 'DeliveryOptions',
-      key: 'id'
+  {
+    defaultScope: {
+      order: [['createdAt', 'ASC']]
     }
-  },
-  createdAt: {
-    type: DataTypes.DATE(3)
-  },
-  updatedAt: {
-    type: DataTypes.DATE(3)
-  },
-}, {
-  defaultScope: {
-    order: [['createdAt', 'ASC']]
   }
-});
+)
