70e7bac8fcd2c0462373f57c10a5a9ae053bba08
premdev <prempravash65@gmail.com>
2026-09-12 23:19:10 +0530

    Update zipFiles.js

--- ecommerce-backend/zipFiles.js
+++ ecommerce-backend/zipFiles.js
@@ -1,32 +1,40 @@
-import fs from 'fs';
-import path from 'path';
-import archiver from 'archiver';
+import fs from 'fs'
+import path from 'path'
+import archiver from 'archiver'
 
-const outputDir = path.resolve('./');
-const zipFilePrefix = 'ecommerce-backend-';
+const outputDir = path.resolve('./')
+const zipFilePrefix = 'ecommerce-backend-'
 
 // Get the next zip file ID
 const getNextZipId = () => {
-  const files = fs.readdirSync(outputDir);
-  const zipFiles = files.filter(file => file.startsWith(zipFilePrefix) && file.endsWith('.zip'));
-  const ids = zipFiles.map(file => parseInt(file.replace(zipFilePrefix, '').replace('.zip', ''), 10)).filter(Number.isInteger);
-  return ids.length > 0 ? Math.max(...ids) + 1 : 1;
-};
+  const files = fs.readdirSync(outputDir)
+  const zipFiles = files.filter(
+    file => file.startsWith(zipFilePrefix) && file.endsWith('.zip')
+  )
+  const ids = zipFiles
+    .map(file =>
+      parseInt(file.replace(zipFilePrefix, '').replace('.zip', ''), 10)
+    )
+    .filter(Number.isInteger)
+  return ids.length > 0 ? Math.max(...ids) + 1 : 1
+}
 
-const zipId = getNextZipId();
-const zipFileName = `${zipFilePrefix}${zipId}.zip`;
-const output = fs.createWriteStream(path.join(outputDir, zipFileName));
-const archive = archiver('zip', { zlib: { level: 9 } });
+const zipId = getNextZipId()
+const zipFileName = `${zipFilePrefix}${zipId}.zip`
+const output = fs.createWriteStream(path.join(outputDir, zipFileName))
+const archive = archiver('zip', { zlib: { level: 9 } })
 
 output.on('close', () => {
-  console.log(`${zipFileName} has been created. Total size: ${archive.pointer()} bytes`);
-});
+  console.log(
+    `${zipFileName} has been created. Total size: ${archive.pointer()} bytes`
+  )
+})
 
 archive.on('error', err => {
-  throw err;
-});
+  throw err
+})
 
-archive.pipe(output);
+archive.pipe(output)
 
 // Add files and folders to the archive using glob patterns
 archive.glob('**/*', {
@@ -37,7 +45,7 @@ archive.glob('**/*', {
     'database.sqlite', // Exclude database.sqlite
     `${zipFilePrefix}*.zip` // Exclude previous zip files
   ]
-});
+})
 
 // Finalize the archive
-archive.finalize();
+archive.finalize()
