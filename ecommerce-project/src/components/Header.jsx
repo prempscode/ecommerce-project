c3cc7e1298e9f84e84e3e723dd9876757eac5899
premdev <prempravash65@gmail.com>
2026-09-12 23:19:25 +0530

    Update Header.jsx

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -1,43 +1,45 @@
-import { NavLink } from "react-router";
-import "./header.css";
-const Header = ({cart}) => {
+import { NavLink } from 'react-router'
+import './header.css'
+const Header = ({ cart }) => {
+  let totalQuantity = 0
 
-  let totalQuantity = 0;
-
-  cart.forEach((cartItem)=>{
-    totalQuantity += cartItem.quantity;
+  cart.forEach(cartItem => {
+    totalQuantity += cartItem.quantity
   })
 
   return (
-    <div className="header">
-      <div className="left-section">
-        <NavLink to="/" className="header-link">
-          <img className="logo" src="images/blue-silkroadmaster-logo.png" />
-          <img className="mobile-logo" src="images/blue-silkroadmaster-logo.png" />
+    <div className='header'>
+      <div className='left-section'>
+        <NavLink to='/' className='header-link'>
+          <img className='logo' src='images/blue-silkroadmaster-logo.png' />
+          <img
+            className='mobile-logo'
+            src='images/blue-silkroadmaster-logo.png'
+          />
         </NavLink>
       </div>
 
-      <div className="middle-section">
-        <input className="search-bar" type="text" placeholder="Search" />
+      <div className='middle-section'>
+        <input className='search-bar' type='text' placeholder='Search' />
 
-        <button className="search-button">
-          <img className="search-icon" src="images/icons/search-icon.png" />
+        <button className='search-button'>
+          <img className='search-icon' src='images/icons/search-icon.png' />
         </button>
       </div>
 
-      <div className="right-section">
-        <NavLink className="orders-link header-link" to="/orders">
-          <span className="orders-text">Orders</span>
+      <div className='right-section'>
+        <NavLink className='orders-link header-link' to='/orders'>
+          <span className='orders-text'>Orders</span>
         </NavLink>
 
-        <NavLink className="cart-link header-link" to="/checkout">
-          <img className="cart-icon" src="images/icons/cart-icon.png" />
-          <div className="cart-quantity">{totalQuantity}</div>
-          <div className="cart-text">Cart</div>
+        <NavLink className='cart-link header-link' to='/checkout'>
+          <img className='cart-icon' src='images/icons/cart-icon.png' />
+          <div className='cart-quantity'>{totalQuantity}</div>
+          <div className='cart-text'>Cart</div>
         </NavLink>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
