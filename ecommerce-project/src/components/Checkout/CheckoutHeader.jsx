120e06eda6148c94d6f71e89b436d8bc2508ba3b
premdev <prempravash65@gmail.com>
2026-09-12 23:19:19 +0530

    Update CheckoutHeader.jsx

--- ecommerce-project/src/components/Checkout/CheckoutHeader.jsx
+++ ecommerce-project/src/components/Checkout/CheckoutHeader.jsx
@@ -1,32 +1,32 @@
-import { Link } from "react-router";
-import "./CheckoutHeader.css";
+import { Link } from 'react-router'
+import './CheckoutHeader.css'
 const CheckoutHeader = () => {
   return (
     <>
-      <div className="checkout-header">
-        <div className="header-content">
-          <div className="checkout-header-left-section">
-            <Link to="index.html">
-              <img clasName="logo" src="images/" />
-              <img className="mobile-logo" src="images/" />
+      <div className='checkout-header'>
+        <div className='header-content'>
+          <div className='checkout-header-left-section'>
+            <Link to='index.html'>
+              <img clasName='logo' src='images/' />
+              <img className='mobile-logo' src='images/' />
             </Link>
           </div>
 
-          <div className="checkout-header-middle-section">
+          <div className='checkout-header-middle-section'>
             Checkout (
-            <Link className="return-to-home-link" to="index.html">
+            <Link className='return-to-home-link' to='index.html'>
               3 items
             </Link>
             )
           </div>
 
-          <div className="checkout-header-right-section">
-            <img src="images/icons/checkout-lock-icon.png" />
+          <div className='checkout-header-right-section'>
+            <img src='images/icons/checkout-lock-icon.png' />
           </div>
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default CheckoutHeader;
+export default CheckoutHeader
